1a2f78933db98e3d753db2dd20fc5a997cdbdd1f
qsdesa322 <qsdesa322@naver.com>
2018-02-24 19:55:46 +0900

    start Design Pattern

---
+++ src/com/DesignPatternExample/Main.java
@@ -0,0 +1,12 @@
+package com.DesignPatternExample;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+        System.out.println("OK!!!");
+
+    }
+
+}
